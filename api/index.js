2e585f76bbbc8f72ab0c986f7341cb979db829db
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-27 10:02:01 -0300

    chore: update shareCode to J50GKF2MCVX

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=0KEYK5IUE1X&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=J50GKF2MCVX&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }

468637704e2312dd1e10f4976495c0662c2f12d6
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-28 11:09:21 -0300

    chore: update shareCode to TYXBKY20RMG

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=J50GKF2MCVX&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=TYXBKY20RMG&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }

cf2c3781060d79d9ab8ecaa09cd7632f83c144c8
JCPrime01 <vanzokiller@gmail.com>
2026-05-29 10:35:16 -0300

    chore: shareCode TYXBKY20RMG -> S9CZKOACG4U (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=TYXBKY20RMG&afp=bingo' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/xkczsekz?shareCode=S9CZKOACG4U&afp=bingo' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
